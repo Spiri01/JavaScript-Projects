ce0404cc4fefc0c71770c0346e5760b07a0fbcb4
Spiri01 <106698746+Spiri01@users.noreply.github.com>
2022-07-30 15:52:46 +0100

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -4,4 +4,4 @@ document.write("Knock it off, Bart! Or I'll tell dad\"<br>\"Eat my shorts!\" Bar
 var A = "Dad hear what Bart told and started to strangle him !<br>";
 var A = A.fontcolor ("Green") //Using the fontcolor tu make the writing blue
 document.write(A)
-document.write(333 + 333); //This is an expresion 
+document.write(333 + 333); //This is an expresion.

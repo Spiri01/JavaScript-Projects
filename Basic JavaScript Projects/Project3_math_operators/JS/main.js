386717f8b7dc9a9e059de035b3449b066670926d
Spiri01 <106698746+Spiri01@users.noreply.github.com>
2022-08-06 17:44:44 +0100

    ....

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,4 +1,8 @@
 function toCelsius(fahrenheit) {
     return (5/9) * (fahrenheit-32);
   }
-  document.getElementById("demo").innerHTML = toCelsius(77);
+  document.getElementById("demo").innerHTML = "The temperature is " + toCelsius(77) + " Celsius";
+
+function subtraction_Function() {
+    document.write("-Click to check the date and time in your location <br>");
+}

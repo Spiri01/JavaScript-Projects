0076309fada8113d134f1d0a06e0529a8ddcf264
Spiri01 <106698746+Spiri01@users.noreply.github.com>
2023-02-17 20:10:37 +0000

    new

--- Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -24,12 +24,13 @@ function getReceipt(runningTotal,sizeTotal) {
     } else if (selectedSize === "Happy Special Pizza"){
         sizeTotal = 34;
     }
-    runningTotal = sizeTotal;
+    runningTotal = sizeTotal = buyngTotal;
     console.log(selectedSize+"= $"+sizeTotal+".00");
     console.log("size text1:"+text1);
     console.log("subtotal: $"+runningTotal+".00");
     getTopping(runningTotal,text1);
 };
+
 function getTopping(runningTotal,text1){
     var toppingTotal = 0;
     var selectedTopping = []
@@ -39,21 +40,47 @@ function getTopping(runningTotal,text1){
             selectedTopping.push (toppingArray[j].value);
             console.log("slected topping item: ("+toppingArray[j].value+")");
             text1 = text1+toppingArray[j].value+"<br>";
-        
+			var toppingCount = selectedTopping.length;
+			if (toppingCount>1) {
+				toppingTotal = (toppingCount - 1);
+			} else {
+				toppingTotal = 0;
+			}
+			runningTotal = (runningTotal + toppingTotal + buyngTotal);
+			console.log("total selected topping items: "+toppingCount);
+			console.log(toppingCount+"topping - 1 free topping = "+"$"+toppingTotal+".00");
+			console.log("topping text1: "+text1);
+			console.log("Purchase Total: "+"$"+runningTotal+".00");
+			document.getElementById("showText").innerHTML=text1;
+			document.getElementById("totalPrice").innerHTML="</h3>Total: <strong>$"+
+			runningTotal+".00"+"</strong></h3>";
         }
-}
-    var toppingCount = selectedTopping.length;
-    if (toppingCount>1) {
-        toppingTotal = (toppingCount - 1);
+	}
+};
+function getVegetables(buyngTotal,text1){
+            var vegetableTotal = 0;
+            var selectedVegetable = []
+            var vegetableArray =document.getElementsByClassName("vegetable");
+            for (var k = 0; k <vegetableArray.length; k++) {
+                if (vegetableArray[k].checked){
+                    selectedVegetable.push (vegetableArray[k].value);
+                    console.log("slected vegetable item: ("+vegetableArray[k].value+")");
+                    text1 = text1+vegetableArray[k].value+"<br>";
+			
+    var vegetableCount = selectedVegetable.length;
+    if (vegetableCount>1) {
+        vegetableTotal = (vegetableCount - 1);
     } else {
-        toppingTotal = 0;
+        vegetableTotal = 0;
     }
-    runningTotal = (runningTotal + toppingTotal);
-    console.log("total selected topping items: "+toppingCount);
-    console.log(toppingCount+"topping - 1 free topping = "+"$"+toppingTotal+".00");
-    console.log("topping text1: "+text1);
+    runningTotal = (runningTotal + toppingTotal + buyngTotal);
+    console.log("total selected vegetable items: "+vegetableCount);
+    console.log(vegetableCount+"vegetable - 1 free topping = "+"$"+vegetableTotal+".00");
+    console.log("vegetable text1: "+text1);
     console.log("Purchase Total: "+"$"+runningTotal+".00");
     document.getElementById("showText").innerHTML=text1;
     document.getElementById("totalPrice").innerHTML="</h3>Total: <strong>$"+
     runningTotal+".00"+"</strong></h3>";
+		}
+	}
 };
